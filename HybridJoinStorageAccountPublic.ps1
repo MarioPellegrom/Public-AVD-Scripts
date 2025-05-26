5656ce927fd5422ff24ecb028c376b4a1e62f2c3
HobbyHok <mario_pellegrom@hotmail.com>
2025-05-26 10:26:50 +0200

    Add files via upload

---
+++ HybridJoinStorageAccountPublic.ps1
@@ -0,0 +1,37 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+
+cd C:\temp\AzFilesHybrid
+.\CopyToPSPath.ps1
+
+Import-Module -Name azfileshybrid
+
+Connect-AzAccount 
+
+$SubscriptionId = "Zet hier je subscription ID" # ID van de Subscription waar je storageaccount in zit
+$ResourceGroupName = "ResourceGroup Name" # Resource group naam waar je storage account staat
+$StorageAccountName = "StorageAccountName" # Naam van je storage account in Azure
+$SamAccountName = "ADDS SAM AccountName" # Geef een naam voor je storage account in ADDS
+$DomainAccountType = "ComputerAccount" # kan zo blijven staan 
+$OuDistinguishedName = "OU to put the storageaccount" # Example OU=Storage Accounts,OU=Pipo,DC=Pipo,DC=nl
+
+Select-AzSubscription -SubscriptionId $SubscriptionId
+
+Join-AzStorageAccount `
+        -ResourceGroupName $ResourceGroupName `
+        -StorageAccountName $StorageAccountName `
+        -SamAccountName $SamAccountName `
+        -DomainAccountType $DomainAccountType `
+        -OrganizationalUnitDistinguishedName $OuDistinguishedName
+
+Debug-AzStorageAccountAuth -StorageAccountName $StorageAccountName -ResourceGroupName $ResourceGroupName -Verbose
+
+# Get the target storage account
+$storageaccount = Get-AzStorageAccount `
+        -ResourceGroupName "IP_Demo_Environment" `
+        -Name "ipdemostorageaccount"
+
+# List the directory service of the selected service account
+$storageAccount.AzureFilesIdentityBasedAuth.DirectoryServiceOptions
+
+# List the directory domain information if the storage account has enabled AD DS authentication for file shares
+$storageAccount.AzureFilesIdentityBasedAuth.ActiveDirectoryProperties
